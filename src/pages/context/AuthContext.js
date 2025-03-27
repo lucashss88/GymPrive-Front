2c5438a8162a31ef782e5cf0445b97f87fc9bf11
lucashss88 <lucashss88@gmail.com>
2025-03-27 15:04:06 -0300

    new version 2025.2

--- src/pages/context/AuthContext.js
+++ src/pages/context/AuthContext.js
@@ -17,7 +17,7 @@ export const AuthProvider = ({ children }) => {
                 const token = localStorage.getItem('token');
                 if (token) {
                     axios.defaults.headers.common['x-auth-token'] = token;
-                    const response = await axios.get('https://localhost:3001/auth/me');
+                    const response = await axios.get('http://localhost:3001/auth/me');
                     setUser(response.data.user);
                     setNameUser(response.data.user.name);
                 } else {
@@ -35,7 +35,7 @@ export const AuthProvider = ({ children }) => {
 
     const register = async (name, email, password, weight, height, age) => {
         try {
-            const response = await axios.post('https://localhost:3001/auth/register', {
+            const response = await axios.post('http://localhost:3001/auth/register', {
                 name,
                 email,
                 password,
@@ -58,7 +58,7 @@ export const AuthProvider = ({ children }) => {
 
     const login = async (email, password) => {
         try {
-            const response = await axios.post('https://localhost:3001/auth/login', { email, password });
+            const response = await axios.post('http://localhost:3001/auth/login', { email, password });
             const { token, user } = response.data;
             localStorage.setItem('token', token);
             axios.defaults.headers.common['x-auth-token'] = token;
@@ -89,7 +89,7 @@ export const AuthProvider = ({ children }) => {
             const token = localStorage.getItem('token');
             if (token) {
                 axios.defaults.headers.common['x-auth-token'] = token;
-                const response = await axios.put('https://localhost:3001/auth/me', updatedData);
+                const response = await axios.put('http://localhost:3001/auth/me', updatedData);
                 setUser(response.data.user);
                 setNameUser(response.data.user.name);
             } else {
@@ -104,7 +104,7 @@ export const AuthProvider = ({ children }) => {
 
     const resetPassword = async (email) => {
         try {
-            await axios.post('https://localhost:3001/auth/reset-password', { email });
+            await axios.post('http://localhost:3001/auth/reset-password', { email });
             toast.success('Instruções de redefinição de senha enviadas.');
         } catch (error) {
             console.error('Erro ao solicitar redefinição de senha', error.response ? error.response.data : error);
